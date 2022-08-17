f0e6766865ab73d952db834c32668bd67c65738e
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2022-08-17 20:44:18 +0100

    add script for nav functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navMenuBtn = document.querySelector("#menu-btn");
+const navMenuIcon = document.querySelector("#menu-icon");
+
+navMenuBtn.addEventListener("click", () => {
+  if (primaryNav.style.display == "flex") {
+    primaryNav.style.display = "none";
+    navMenuIcon.classList.remove("fa-times");
+    navMenuIcon.classList.add("fa-bars");
+  } else {
+    primaryNav.style.display = "flex";
+    navMenuIcon.classList.remove("fa-bars");
+    navMenuIcon.classList.add("fa-times");
+  }
+});
